4d7583626cf3cc9fd4674c46b75e89d7f633aecf
Obaremi <obaremimuyiwa@gmail.com>
2024-05-30 13:56:50 +0100

    commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Colorpicker from './Colorpicker'
+import './App.css'
+
+function App() {
+  return(
+    <>
+  <Colorpicker />
+    </>
+  )
+}
+
+export default App
---
+++ src/Colorpicker.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+const Colorpicker = () => {
+    let [color, setColor] = useState('#FF4FOO')
+  return (
+    <div className='color-'>
+      <h1> Color Picker</h1>
+          </div>
+  )
+}
+
+export default Colorpicker
