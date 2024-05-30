8cf724f26448cdef7aafe20d82617508a2183702
Obaremi <obaremimuyiwa@gmail.com>
2024-05-30 13:58:20 +0100

    commit

--- src/Colorpicker.jsx
+++ src/Colorpicker.jsx
@@ -3,8 +3,11 @@ import React, {useState} from 'react'
 const Colorpicker = () => {
     let [color, setColor] = useState('#FF4FOO')
   return (
-    <div className='color-'>
+    <div className='color-picker-container'>
       <h1> Color Picker</h1>
+      <div className='color-display'>
+        
+      </div>
           </div>
   )
 }
