308d4852f629a9e971b347bc687c07e1a55e478a
Obaremi <obaremimuyiwa@gmail.com>
2024-06-04 10:03:02 +0100

    update css

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
-import benten from './assets/benten.jpg'
+import benten from "./assets/benten.jpg";
 
 const Todo = () => {
-  let [tasks, setTasks] = useState(['take a shower', 'play fifa', 'read manwha']);
+  let [tasks, setTasks] = useState([
+    "take a shower",
+    "play fifa",
+    "read manwha",
+  ]);
   let [newTasks, setnewTasks] = useState("");
 
   const handleTask = (event) => {
@@ -17,25 +21,35 @@ const Todo = () => {
 
   const movetaskdown = () => {};
   return (
-    <div className="to-do">
+    <div className="to-do-list">
       <img className="img" src={benten}></img>
       <h1> To-Do List</h1>
-      
+
       <div>
         <input
           type="text"
           placeholder="enter a task..."
           onChange={handleTask}
           value={newTasks}
-        /> <br></br>
+        />{" "}
+        <br></br>
         <button className="add-button">Add</button>
       </div>
       <ol>
-        {tasks.map((task, index)=>
-        <li key={index}>
-        <span className="text">{task}</span>
-        <button className="delete-buuton" onClick={deleteTask}>❌</button>
-        </li>)}
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span className="text">{task}</span>
+            <button className="delete-button" onClick={() => deleteTask(index)}>
+              ❌
+            </button>
+            <button className="move-button" onClick={() => movetaskup(index)}>
+              ☝️
+            </button>
+            <button className="move-button" onClick={() => movetaskdown(index)}>
+              👇
+            </button>
+          </li>
+        ))}
       </ol>
     </div>
   );
