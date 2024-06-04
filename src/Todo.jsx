65ca1f7ac07d2c7fb4c9745533180fe186e48c62
Obaremi <obaremimuyiwa@gmail.com>
2024-06-04 08:55:05 +0100

    update

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,13 +1,40 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 const Todo = () => {
+  let [tasks, setTasks] = useState(['take a shower', 'play fifa', 'read manwha']);
+  let [newTasks, setnewTasks] = useState("");
 
-  let [tasks, setTasks] = useState([])
+  const handleTask = (event) => {
+    setnewTasks(event.target.value);
+  };
+
+  const addTask = () => {};
+
+  const deleteTask = () => {};
+
+  const movetaskup = () => {};
+
+  const movetaskdown = () => {};
   return (
-    <div>
-      
+    <div className="to-do">
+      <h1> To-Do List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="enter a task..."
+          onChange={handleTask}
+          value={newTasks}
+        /> <br></br>
+        <button className="add-button">Add</button>
+      </div>
+      <ol>
+        {tasks.map((task, index)=>
+        <li key={index}>
+        <span className="text">{task}</span>
+        </li>)}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
